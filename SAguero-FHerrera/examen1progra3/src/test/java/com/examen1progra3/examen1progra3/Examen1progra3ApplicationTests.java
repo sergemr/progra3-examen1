b5575bcc25ccd1720864c1aecf22999d8455d51d
Sergio Aguero <sergioaguerovalle@gmail.com>
2023-10-24 19:02:11 -0600

    SergioAguero-FabricioHerrera1

---
+++ SAguero-FHerrera/examen1progra3/src/test/java/com/examen1progra3/examen1progra3/Examen1progra3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen1progra3.examen1progra3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen1progra3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
