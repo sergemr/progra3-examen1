923ea45b40a97afb3555e647d5aedda56fb5c048
nullb3 <dasistnullbe@gmail.com>
2023-10-24 19:06:43 -0600

    Updated Pedido.java

--- amontiel-lrua/src/main/java/examenmontielrua/examen/Pedido.java
+++ amontiel-lrua/src/main/java/examenmontielrua/examen/Pedido.java
@@ -2,4 +2,27 @@ package examenmontielrua.examen;
 
 public class Pedido {
     
+    private int Precio;
+    private int Cantidad;
+    private String Descripcion;
+
+    public int getPrecio() {
+        return Precio;
+    }
+    public int getCantidad(){
+        return Cantidad;
+    }
+    public String getDescripcion(){
+        return Descripcion;
+    }
+
+    public void setPrecio(int Precio){
+        this.Precio = Precio;
+    }
+    public void setCantidad(int Cantidad){
+        this.Cantidad = Cantidad;
+    }
+    public void setDescripcion(String Descripcion){
+        this.Descripcion = Descripcion;
+    }
 }
